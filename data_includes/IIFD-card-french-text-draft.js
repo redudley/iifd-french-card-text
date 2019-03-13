7e76b4563c4e1d915a680e226acdf7a37cff1a26
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2019-03-13 14:41:05 +0100

    big changes

--- data_includes/IIFD-card-french-text-draft.js
+++ data_includes/IIFD-card-french-text-draft.js
@@ -31,7 +31,7 @@ var items = [
 //        "FrenchMessage",  {html: {include: 'domain.html'}},
 //        "FrenchMessage",  {html: {include: 'clue_two.html'}},
 //        "FrenchMessage",  {html: {include: 'clue_two.html'}},
-        "FrenchKidAccept", {html: {include:'image_choice.html'},
+        "FrenchKidAccept", {s:'image_choice.html',
                       as:   [["D","https://imgur.com/wBdDYxX.png"], //4c4s - true
                             ["K","https://imgur.com/hPPmspP.png"]]},]
 
